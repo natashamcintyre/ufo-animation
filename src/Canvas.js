2428488fe06508388d3b7a0497746e9ae47fecca
Natasha McIntyre <natashamc.cs@gmail.com>
2021-04-06 10:06:13 +0100

    Set up canvas

---
+++ src/Canvas.js
@@ -0,0 +1,11 @@
+function Canvas() {
+    return(
+      <div className="App">
+        <canvas
+          style={{ width: '800px', height: '450px' }}
+        />
+      </div>
+    )
+}
+
+export default Canvas
